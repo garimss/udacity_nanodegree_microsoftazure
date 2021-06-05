b7e2488de5c864569598d38fcdffb54515cf017f
garimss <garima.rga@gmail.com>
2021-06-05 11:16:51 +0700

    Login successful prints

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -61,20 +61,23 @@ def post(id):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:)
+    if current_user.is_authenticated:
+        flash('Login Successful')
+        app.logger.error('Login Successful')
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            app.logger.error('Login Failed: Invalid username or password')
+            app.logger.error('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        app.logger.info('Login Successful: User logged in, {}'.format(form.username.data))
+        app.logger.info('login successful: User logged in, {}'.format(form.username.data))
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
+            #app.logger.info('Login successful')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
